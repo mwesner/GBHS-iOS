244d86149706650cf2541e3798ed06a3cddddfdd
Corey Rowe <CoreyR1196@gmail.com>
2015-04-15 21:59:33 -0400

    Updated app icon set Added UIButtons and AlertAction dialogs to storyboard

--- GBHS/ViewController.swift
+++ GBHS/ViewController.swift
@@ -1,25 +1,74 @@
-//
-//  ViewController.swift
-//  GBHS
-//
-//  Created by student on 4/15/15.
-//  Copyright (c) 2015 Grand Blanc High School. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnAnnounce: UIButton!
+    @IBOutlet weak var btnFacebook: UIButton!
+    @IBOutlet weak var btnTwitter: UIButton!
+    @IBOutlet weak var btnCalendar: UIButton!
+    @IBOutlet weak var btnTimes: UIButton!
+    @IBOutlet weak var btnGrades: UIButton!
+    @IBOutlet weak var btnWeb: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setButtonColor(btnAnnounce)
+        setButtonColor(btnFacebook)
+        setButtonColor(btnTwitter)
+        setButtonColor(btnCalendar)
+        setButtonColor(btnTimes)
+        setButtonColor(btnGrades)
+        setButtonColor(btnWeb)
+    }
+    
+    func setButtonColor(button: UIButton) {
+        button.backgroundColor = UIColor.grayColor()
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.whiteColor().CGColor
+    }
+    
+    @IBAction func showAnnouncements(sender: UIButton) {
+        var alert = UIAlertController(title: "Announcements", message: "Show the announcements!", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func showPosts(sender: UIButton) {
+        var alert = UIAlertController(title: "Facebook", message: "Show 20 posts!", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    @IBAction func showTweets(sender: UIButton) {
+        var alert = UIAlertController(title: "Twitter", message: "Show 20 tweets!", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showCal(sender: UIButton) {
+        var alert = UIAlertController(title: "Calendar", message: "Show the iCal events!", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
     }
-
-
+    
+    
+    @IBAction func showTimes(sender: UIButton) {
+        var alert = UIAlertController(title: "Times", message: "Show the times!", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func checkGrades(sender: UIButton) {
+        var alert = UIAlertController(title: "Grades", message: "Open StudentVUE!", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func showWeb(sender: UIButton) {
+        var alert = UIAlertController(title: "Web", message: "Open the website!", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
 }
 
