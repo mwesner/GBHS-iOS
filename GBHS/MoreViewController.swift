acbe237bf66da754f1c867a974967889021e6b12
Jordon Trower <Leopardclone@gmail.com>
2016-11-04 08:54:03 -0400

    Updated external links, staff members, etc for new year.

--- GBHS/MoreViewController.swift
+++ GBHS/MoreViewController.swift
@@ -9,7 +9,7 @@ class MoreViewController: UITableViewController, UIWebViewDelegate {
     @IBAction func showAbout(sender: UIBarButtonItem) {
         
         let title = "Grand Blanc High School"
-        let message = "Developed by Grand Blanc CTE.\n\nTeam Leader:\n\nMichael Wesner\nAP Computer Science A instructor\n\n2015 Members:\n\nCorey Rowe\nAaron Goodfellow\n\n2016 Members:\n\nMichael Olmsted\nBradley Kivett\n\nIcons provided by Icons8\nhttp://icons8.com"
+        let message = "Developed by Grand Blanc CTE.\n\nTeam Leader:\n\nMichael Wesner\nAP Computer Science A instructor\n\n2015 Members:\n\nCorey Rowe\nAaron Goodfellow\n\n2016 Members:\n\nMichael Olmsted\nBradley Kivett\n\n2017 Members:\n\nJordon Trower\nJared\n\nLily Eckman\nIcons provided by Icons8\nhttp://icons8.com"
         let buttontitle = "Close"
         
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
@@ -79,7 +79,7 @@ class MoreViewController: UITableViewController, UIWebViewDelegate {
         case 2:
             performSegueWithIdentifier("MapSegue", sender: nil)
         case 3:
-            UIApplication.sharedApplication().openURL(NSURL (string: "http://grandblanc.high.schoolfusion.us/modules/calendar/exportICal.php")!)
+            UIApplication.sharedApplication().openURL(NSURL (string: "http://hs.grandblancschools.org/calendar")!)
         case 4:
             performSegueWithIdentifier("SportsSegue", sender: nil)
         case 5:
